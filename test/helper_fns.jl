0e9f2a7cd88f9cafa050a9f7f6b15009c13d5a5a
Olivier Labayle <olabayle@gmail.com>
2023-03-28 14:58:56 +0100

    update helper fns

--- test/helper_fns.jl
+++ test/helper_fns.jl
@@ -1,24 +1,31 @@
 using TMLE
+using Test
+using MLJBase
+using CategoricalArrays
 
-abserrors(results, Ψ₀) = [abs(TMLE.estimate(r) - Ψ₀) for r in results]
+risk(ŷ, y) = rmse(TMLE.expected_value(ŷ), y)
+risk(ŷ, y::CategoricalArray) = mean(log_loss(ŷ, y))
 
-all_tmle_better_than_initial(tmle, initial, Ψ₀) = all(abserrors(tmle, Ψ₀) .<= abserrors(initial, Ψ₀))
-first_better_than_last(results, Ψ₀) = 
-    abs((TMLE.estimate(results[end]) - Ψ₀) / Ψ₀) < abs((TMLE.estimate(results[1]) - Ψ₀) / Ψ₀)
-
-tolerance(res, Ψ₀, tol) = ((TMLE.estimate(res) - Ψ₀) / Ψ₀) < tol
-
-all_solves_ice(tmle_results; tol=1e-10) = all(mean(r.IC) < tol for r in tmle_results)
-
-function asymptotics(Ψ, η_spec, problem_fn, rng, Ns)
-    tmle_results = []
-    initial_results = []
-    Ψ₀ = 0
-    for n in Ns
-        dataset, Ψ₀ = problem_fn(rng; n=n)
-        tmle_result, initial_result, cache = tmle(Ψ, η_spec, dataset; verbosity=0)
-        push!(tmle_results, tmle_result)
-        push!(initial_results, initial_result)
-    end
-    return tmle_results, initial_results, Ψ₀
+function test_fluct_decreases_risk(cache; target_name::Symbol=nothing)
+    y = cache.data[:no_missing][target_name]
+    initial_risk = risk(cache.data[:Q₀], y)
+    fluct_risk = risk(cache.data[:Qfluct], y)
+    @test initial_risk > fluct_risk
 end
+
+function test_fluct_risk_almost_equal_to_initial(cache; target_name::Symbol=nothing, atol=1e-6)
+    y = cache.data[:no_missing][target_name]
+    initial_risk = risk(cache.data[:Q₀], y)
+    fluct_risk = risk(cache.data[:Qfluct], y)
+    @test initial_risk ≈ fluct_risk atol=atol
+end
+
+function test_coverage(tmle_result::TMLE.TMLEResult, Ψ₀)
+    # TMLE
+    lb, ub = confint(OneSampleTTest(tmle_result.tmle))
+    @test lb ≤ Ψ₀ ≤ ub
+    # OneStep
+    lb, ub = confint(OneSampleTTest(tmle_result.onestep))
+    @test lb ≤ Ψ₀ ≤ ub
+end
+
