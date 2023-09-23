efdb459b5ba61c294d59cc5d3bb1375f375a8ecd
Olivier Labayle <olabayle@gmail.com>
2023-09-23 11:25:16 +0100

    fix estimands

--- src/counterfactual_mean_based/estimands.jl
+++ src/counterfactual_mean_based/estimands.jl
@@ -1,201 +1,92 @@
-#####################################################################
-###                      Counterfactual Mean                      ###
-#####################################################################
-
-"""
-# Counterfactual Mean / CM
-
-## Definition
-
-``CM(Y, T=t) = E[Y|do(T=t)]``
-
-## Constructors
-
-- CM(scm::SCM; outcome::Symbol, treatment::NamedTuple)
-- CM(
-    outcome::Symbol, 
-    treatment::NamedTuple, 
-    confounders::Union{Symbol, AbstractVector{Symbol}}, 
-    covariates::Union{Nothing, Symbol, AbstractVector{Symbol}} = nothing, 
-    outcome_model = with_encoder(LinearRegressor()),
-    treatment_model = LinearBinaryClassifier()
+ESTIMANDS_DOCS = Dict(
+    :CM => (formula="``CM(Y, T=t) = E[Y|do(T=t)]``",),
+    :ATE => (formula="``ATE(Y, T, case, control) = E[Y|do(T=case)] - E[Y|do(T=control)``",),
+    :IATE => (formula="``IATE = E[Y|do(T₁=1, T₂=1)] - E[Y|do(T₁=1, T₂=0)] - E[Y|do(T₁=0, T₂=1)] + E[Y|do(T₁=0, T₂=0)]``",)
 )
-## Example
+# Define constructors/name for CMCompositeEstimand types
 
-```julia
-Ψ = CM(scm, outcome=:Y, treatment=(T=1,))
+for (typename, (formula,)) ∈ ESTIMANDS_DOCS
+    ex = quote
+        # """
+        # # $(typename)
 
-```
-"""
-struct CounterfactualMean <: Estimand
-    scm::StructuralCausalModel
-    outcome::Symbol
-    treatment::NamedTuple
-    function CounterfactualMean(scm, outcome, treatment)
-        check_parameter_against_scm(scm, outcome, treatment)
-        return new(scm, outcome, treatment)
+        # ## Definition
+
+        # For two treatments with case/control settings (1, 0):
+
+        # $(formula)
+
+        # ## Constructors
+
+        # - $(typename)(outcome, treatment, confounders; extra_outcome_covariates=Set{Symbol}())
+        # - $(typename)(;
+        #     outcome, 
+        #     treatment, 
+        #     confounders, 
+        #     extra_outcome_covariates 
+        # )
+
+        # ## Example
+
+        # ```julia
+        # Ψ = $(typename)(outcome=:Y, treatment=(T₁=(case=1, control=0), T₂=(case=1,control=0), confounders=[:W₁, :W₂])
+        # ```
+        # """
+        struct $(typename) <: Estimand
+            outcome::Symbol
+            treatment_values::NamedTuple
+            treatment_confounders::NamedTuple
+            outcome_extra_covariates::Tuple{Vararg{Symbol}}
+
+            function $(typename)(outcome, treatment_values, treatment_confounders, outcome_extra_covariates)
+                outcome = Symbol(outcome)
+                treatment_variables = Tuple(keys(treatment_values))
+                treatment_confounders = NamedTuple{treatment_variables}([unique_sorted_tuple(treatment_confounders[T]) for T ∈ treatment_variables])
+                outcome_extra_covariates = unique_sorted_tuple(outcome_extra_covariates)
+                return new(outcome, treatment_values, treatment_confounders, outcome_extra_covariates)
+            end
+        end
+
+        $(typename)(outcome, treatment_values, treatment_confounders; outcome_extra_covariates=()) = 
+            $(typename)(outcome, treatment_values, treatment_confounders, outcome_extra_covariates)
+    
+        $(typename)(;outcome, treatment_values, treatment_confounders, outcome_extra_covariates=()) = 
+            $(typename)(outcome, treatment_values, treatment_confounders, outcome_extra_covariates)
+
+        name(::Type{$(typename)}) = string($(typename))
     end
+    eval(ex)
 end
 
-const CM = CounterfactualMean
+CMCompositeEstimand = Union{(eval(x) for x in keys(ESTIMANDS_DOCS))...}
 
-indicator_fns(Ψ::CM) = Dict(values(Ψ.treatment) => 1.)
-
-
-#####################################################################
-###                  Average Treatment Effect                     ###
-#####################################################################
-"""
-# Average Treatment Effect / ATE
-
-## Definition
-
-``ATE(Y, T, case, control) = E[Y|do(T=case)] - E[Y|do(T=control)``
-
-## Constructors
-
-- ATE(scm::SCM; outcome::Symbol, treatment::NamedTuple)
-- ATE(
-    outcome::Symbol, 
-    treatment::NamedTuple, 
-    confounders::Union{Symbol, AbstractVector{Symbol}}, 
-    covariates::Union{Nothing, Symbol, AbstractVector{Symbol}} = nothing, 
-    outcome_model = with_encoder(LinearRegressor()),
-    treatment_model = LinearBinaryClassifier()
-)
-
-## Example
-
-```julia
-Ψ = ATE(scm, outcome=:Y, treatment=(T=(case=1,control=0),)
-```
-"""
-struct AverageTreatmentEffect <: Estimand
-    scm::StructuralCausalModel
-    outcome::Symbol
-    treatment::NamedTuple
-    function AverageTreatmentEffect(scm, outcome, treatment)
-        check_parameter_against_scm(scm, outcome, treatment)
-        return new(scm, outcome, treatment)
-    end
-end
-
-const ATE = AverageTreatmentEffect
+indicator_fns(Ψ::CM) = Dict(values(Ψ.treatment_values) => 1.)
 
 function indicator_fns(Ψ::ATE)
     case = []
     control = []
-    for treatment in Ψ.treatment
+    for treatment in Ψ.treatment_values
         push!(case, treatment.case)
         push!(control, treatment.control)
     end
     return Dict(Tuple(case) => 1., Tuple(control) => -1.)
 end
 
-#####################################################################
-###            Interaction Average Treatment Effect               ###
-#####################################################################
-
-"""
-# Interaction Average Treatment Effect / IATE
-
-## Definition
-
-For two treatments with case/control settings (1, 0):
-
-``IATE = E[Y|do(T₁=1, T₂=1)] - E[Y|do(T₁=1, T₂=0)] - E[Y|do(T₁=0, T₂=1)] + E[Y|do(T₁=0, T₂=0)]``
-
-## Constructors
-
-- IATE(scm::SCM; outcome::Symbol, treatment::NamedTuple)
-- IATE(
-    outcome::Symbol, 
-    treatment::NamedTuple, 
-    confounders::Union{Symbol, AbstractVector{Symbol}}, 
-    covariates::Union{Nothing, Symbol, AbstractVector{Symbol}} = nothing, 
-    outcome_model = with_encoder(LinearRegressor()),
-    treatment_model = LinearBinaryClassifier()
-)
-
-## Example
-
-```julia
-Ψ = IATE(scm, outcome=:Y, treatment=(T₁=(case=1,control=0), T₂=(case=1,control=0))
-```
-"""
-struct InteractionAverageTreatmentEffect <: Estimand
-    scm::StructuralCausalModel
-    outcome::Symbol
-    treatment::NamedTuple
-    function InteractionAverageTreatmentEffect(scm, outcome, treatment)
-        check_parameter_against_scm(scm, outcome, treatment)
-        return new(scm, outcome, treatment)
-    end
-end
-
-const IATE = InteractionAverageTreatmentEffect
-
-ncases(value, Ψ::IATE) = sum(value[i] == Ψ.treatment[i].case for i in eachindex(value))
+ncases(value, Ψ::IATE) = sum(value[i] == Ψ.treatment_values[i].case for i in eachindex(value))
 
 function indicator_fns(Ψ::IATE)
     N = length(treatments(Ψ))
     key_vals = Pair[]
-    for cf in Iterators.product((values(Ψ.treatment[T]) for T in treatments(Ψ))...)
+    for cf in Iterators.product((values(Ψ.treatment_values[T]) for T in treatments(Ψ))...)
         push!(key_vals, cf => float((-1)^(N - ncases(cf, Ψ))))
     end
     return Dict(key_vals...)
 end
 
-#####################################################################
-###                         Methods                               ###
-#####################################################################
-
-AVAILABLE_ESTIMANDS = (CM, ATE, IATE)
-CMCompositeTypenames = [:CM, :ATE, :IATE]
-CMCompositeEstimand = Union{(eval(x) for x in CMCompositeTypenames)...}
-
-# Define constructors/name for CMCompositeEstimand types
-for typename in CMCompositeTypenames
-    ex = quote
-        name(::Type{$(typename)}) = string($(typename))
-
-        $(typename)(scm::SCM; outcome::Symbol, treatment::NamedTuple) = $(typename)(scm, outcome, treatment)
-        
-        function $(typename)(;
-            outcome::Symbol, 
-            treatment::NamedTuple, 
-            confounders::Union{Symbol, AbstractVector{Symbol}}, 
-            covariates::Union{Nothing, Symbol, AbstractVector{Symbol}} = nothing
-            )
-            scm = StaticConfoundedModel(
-                [outcome], 
-                collect(keys(treatment)), 
-                confounders;
-                covariates=covariates
-                )
-            return $(typename)(scm; outcome=outcome, treatment=treatment)
-        end
-    end
-
-    eval(ex)
-end
-
-VariableNotAChildInSCMError(variable) = ArgumentError(string("Variable ", variable, " is not associated with a Structural Equation in the SCM."))
-TreatmentMustBeInOutcomeParentsError(variable) = ArgumentError(string("Treatment variable ", variable, " must be a parent of the outcome."))
-
-function check_parameter_against_scm(scm::SCM, outcome, treatment)
-    eqs = equations(scm)
-    haskey(eqs, outcome) || throw(VariableNotAChildInSCMError(outcome))
-    for treatment_variable in keys(treatment)
-        haskey(eqs, treatment_variable) || throw(VariableNotAChildInSCMError(treatment_variable))
-        is_upstream(treatment_variable, outcome, scm) || throw(TreatmentMustBeInOutcomeParentsError(treatment_variable))
-    end
-end
-
-function get_relevant_factors(Ψ::CMCompositeEstimand; adjustment_method=BackdoorAdjustment())
-    outcome_model = ExpectedValue(Ψ.scm, outcome(Ψ), outcome_parents(adjustment_method, Ψ))
-    treatment_factors = Tuple(ConditionalDistribution(Ψ.scm, treatment, parents(Ψ.scm, treatment)) for treatment in treatments(Ψ))
-    return CMRelevantFactors(Ψ.scm, outcome_model, treatment_factors)
+function get_relevant_factors(Ψ::CMCompositeEstimand)
+    outcome_model = ExpectedValue(Ψ.outcome, Tuple(union(Ψ.outcome_extra_covariates, keys(Ψ.treatment_confounders), (Ψ.treatment_confounders)...)))
+    treatment_factors = Tuple(ConditionalDistribution(T, Ψ.treatment_confounders[T]) for T in treatments(Ψ))
+    return CMRelevantFactors(outcome_model, treatment_factors)
 end
 
 function Base.show(io::IO, ::MIME"text/plain", Ψ::T) where T <: CMCompositeEstimand 
@@ -203,23 +94,7 @@ function Base.show(io::IO, ::MIME"text/plain", Ψ::T) where T <: CMCompositeEsti
         name(T),
         "\n-----",
         "\nOutcome: ", Ψ.outcome,
-        "\nTreatment: ", Ψ.treatment
+        "\nTreatment: ", Ψ.treatment_values
     )
     println(io, param_string)
-end
-
-outcome_equation(Ψ::CMCompositeEstimand) = Ψ.scm[outcome(Ψ)]
-
-get_outcome_model(Ψ::CMCompositeEstimand) = outcome_equation(Ψ).mach
-
-get_outcome_datas(Ψ::CMCompositeEstimand) = get_outcome_model(Ψ).data
-
-outcome(Ψ::CMCompositeEstimand) = Ψ.outcome
-outcome(dataset, Ψ::CMCompositeEstimand) = Tables.getcolumn(dataset, outcome(Ψ))
-
-function estimand_key(Ψ::CMCompositeEstimand)
-    return (
-        join(treatments(Ψ), "_"),
-        string(outcome(Ψ)),
-    )
 end
--- test/counterfactual_mean_based/estimands.jl
+++ test/counterfactual_mean_based/estimands.jl
@@ -8,257 +8,8 @@ using StableRNGs
 using MLJBase
 using MLJGLMInterface
 
-@testset "Test various methods" begin
-    n = 5
-    dataset = (
-        Y = rand(n),
-        T = categorical([0, 1, 1, 0, 0]),
-        W₁ = rand(n),
-        W₂ = rand(n)
-    )
-    # CM
-    scm = StaticConfoundedModel(
-        :Y, :T, [:W₁, :W₂], 
-        covariates=:C₁
-    )
-    # Parameter validation
-    @test_throws TMLE.VariableNotAChildInSCMError("UndefinedOutcome") CM(
-        scm,
-        treatment=(T=1,),
-        outcome=:UndefinedOutcome
-    )
-    @test_throws TMLE.VariableNotAChildInSCMError("UndefinedTreatment") CM(
-        scm,
-        treatment=(UndefinedTreatment=1,),
-        outcome=:Y
-    )
-    @test_throws TMLE.TreatmentMustBeInOutcomeParentsError("Y") CM(
-        scm,
-        treatment=(Y=1,),
-        outcome=:T
-    )
 
-    Ψ = CM(
-        scm,
-        treatment=(T=1,),
-        outcome =:Y
-    )
-    
-    @test TMLE.treatments(Ψ) == [:T]
-    @test TMLE.treatments(dataset, Ψ) == (T=dataset.T,)
-    @test TMLE.outcome(Ψ) == :Y
-    
-    # ATE
-    scm = SCM(
-        SE(:Z, [:T₁, :T₂, :W₁₁, :W₁₂, :W₂₁]),
-        SE(:T₁, [:W₁₁, :W₁₂]),
-        SE(:T₂, [:W₂₁]),
-    )
-    Ψ = ATE(
-        scm,
-        treatment=(T₁=(case=1, control=0), T₂=(case="AC", control="CC")),
-        outcome =:Z,
-    )
-    @test TMLE.treatments(Ψ) == [:T₁, :T₂]
-    @test TMLE.outcome(Ψ) == :Z
-
-    # IATE
-    Ψ = IATE(
-        scm,
-        treatment=(T₁=(case=1, control=0), T₂=(case="AC", control="CC")),
-        outcome =:Z,
-    )
-    @test TMLE.treatments(Ψ) == [:T₁, :T₂]
-    @test TMLE.outcome(Ψ) == :Z
-end
-
-@testset "Test constructors" begin
-    Ψ = CM(outcome=:Y, treatment=(T=1,), confounders=[:W])
-    @test parents(Ψ.scm, :Y) == Set([:T, :W])
-
-    Ψ = ATE(outcome=:Y, treatment=(T₁=(case=1, control=0), T₂=(case=1, control=0)), confounders=:W)
-    @test parents(Ψ.scm, :Y) == Set([:T₁, :T₂, :W])
-
-    Ψ = IATE(outcome=:Y, treatment=(T₁=(case=1, control=0), T₂=(case=1, control=0)), confounders=:W)
-    @test parents(Ψ.scm, :Y) == Set([:T₁, :T₂, :W])
-end
-
-
-@testset "Test fit!" begin
-    rng = StableRNG(123)
-    n = 100
-    dataset = (
-        Ycat = categorical(rand(rng, [0, 1], n)),
-        Ycont = rand(rng, n),
-        T₁ = categorical(rand(rng, [0, 1], n)),
-        T₂ = categorical(rand(rng, [0, 1], n)),
-        W₁₁ = rand(rng, n),
-        W₁₂ = rand(rng, n),
-        W₂₁ = rand(rng, n),
-        W₂₂ = rand(rng, n),
-        C = rand(rng, n)
-    )
-
-    scm = SCM(
-        SE(:Ycat, [:T₁, :T₂, :W₁₁, :W₁₂, :W₂₁, :W₂₂, :C]),
-        SE(:Ycont, [:T₁, :T₂, :W₁₁, :W₁₂, :W₂₁, :W₂₂, :C]),
-        SE(:T₁, [:W₁₁, :W₁₂]),
-        SE(:T₂, [:W₂₁, :W₂₂]),
-    )
-    set_conditional_distribution!(scm, :Ycat, with_encoder(LinearBinaryClassifier()))
-    set_conditional_distribution!(scm, :Ycont, with_encoder(LinearRegressor()))
-    set_conditional_distribution!(scm, :T₁, LinearBinaryClassifier())
-    set_conditional_distribution!(scm, :T₂, LinearBinaryClassifier())
-    # Fits CM required factors
-    Ψ = CM(
-        scm,
-        treatment=(T₁=1,),
-        outcome=:Ycat
-    )
-    log_sequence = (
-        (:info, TMLE.UsingNaturalDistributionLog(scm, :Ycat, Set([:W₁₂, :W₁₁, :T₁, :C]))),
-        (:info, "Fitting Conditional Distribution Factor: Ycat | W₁₂, W₁₁, T₁, C"),
-        (:info, "Fitting Conditional Distribution Factor: T₁ | W₁₂, W₁₁"),
-    )
-    @test_logs log_sequence... fit!(Ψ, dataset; adjustment_method=BackdoorAdjustment([:C]), verbosity=1)
-    outcome_dist = get_conditional_distribution(scm, :Ycat, Set([:W₁₂, :W₁₁, :T₁, :C]))
-    @test keys(fitted_params(outcome_dist.machine)) == (:linear_binary_classifier, :treatment_transformer)
-    @test keys(outcome_dist.machine.data[1]) == (:W₁₂, :W₁₁, :T₁, :C)
-    treatment_dist = get_conditional_distribution(scm, :T₁, Set([:W₁₁, :W₁₂]))
-    @test fitted_params(treatment_dist.machine).features == [:W₁₂, :W₁₁]
-    @test keys(treatment_dist.machine.data[1]) == (:W₁₂, :W₁₁)
-    # The natural outcome distribution has not been fitted for instance
-    natural_outcome_dist = get_conditional_distribution(scm, :Ycat, Set([:T₂, :W₁₂, :W₂₂, :W₂₁, :W₁₁, :T₁, :C]))
-    @test !isdefined(natural_outcome_dist, :machine)
-
-    # Fits ATE required equations that haven't been fitted yet.
-    Ψ = ATE(
-        scm,
-        treatment=(T₁=(case=1, control=0), T₂=(case=1, control=0)),
-        outcome =:Ycat,
-    )
-    conditioning_set = Set([:W₁₂, :W₂₂, :W₂₁, :W₁₁, :T₁, :T₂])
-    log_sequence = (
-        (:info, TMLE.UsingNaturalDistributionLog(scm, :Ycat, conditioning_set)),
-        (:info, "Fitting Conditional Distribution Factor: Ycat | W₁₂, W₂₂, W₂₁, W₁₁, T₁, T₂"),
-        (:info, "Reusing or Updating Conditional Distribution Factor: T₁ | W₁₂, W₁₁"),
-        (:info, "Fitting Conditional Distribution Factor: T₂ | W₂₂, W₂₁"),
-    )
-    @test_logs log_sequence... fit!(Ψ, dataset, verbosity=1)
-
-    outcome_dist = get_conditional_distribution(scm, :Ycat, conditioning_set)
-    @test keys(fitted_params(outcome_dist.machine)) == (:linear_binary_classifier, :treatment_transformer)
-    @test Set(keys(outcome_dist.machine.data[1])) == conditioning_set
-    treatment_dist = get_conditional_distribution(scm, :T₂, Set([:W₂₂, :W₂₁]))
-    @test fitted_params(treatment_dist.machine).features == [:W₂₂, :W₂₁]
-    @test keys(treatment_dist.machine.data[1]) == (:W₂₂, :W₂₁)
-    
-    # Fits IATE required equations that haven't been fitted yet.
-    Ψ = IATE(
-        scm,
-        treatment=(T₁=(case=1, control=0), T₂=(case=1, control=0)),
-        outcome =:Ycont,
-    )
-    conditioning_set = Set([:W₁₂, :W₂₂, :W₂₁, :W₁₁, :T₁, :T₂])
-    log_sequence = (
-        (:info, TMLE.UsingNaturalDistributionLog(scm, :Ycont, conditioning_set)),
-        (:info, "Fitting Conditional Distribution Factor: Ycont | W₁₂, W₂₂, W₂₁, W₁₁, T₁, T₂"),
-        (:info, "Reusing or Updating Conditional Distribution Factor: T₁ | W₁₂, W₁₁"),
-        (:info, "Reusing or Updating Conditional Distribution Factor: T₂ | W₂₂, W₂₁"),
-    )
-    @test_logs log_sequence... fit!(Ψ, dataset, verbosity=1)
-    outcome_dist = get_conditional_distribution(scm, :Ycont, conditioning_set)
-    @test keys(fitted_params(outcome_dist.machine)) == (:linear_regressor, :treatment_transformer)
-    @test Set(keys(outcome_dist.machine.data[1])) == conditioning_set
-
-end
-
-@testset "Test optimize_ordering" begin
-    rng = StableRNG(123)
-    scm = SCM(
-        SE(:T₁, [:W₁, :W₂]),
-        SE(:T₂, [:W₁, :W₂, :W₃]),
-        SE(:Y₁, [:T₁, :T₂, :W₁, :W₂, :C₁]),
-        SE(:Y₂, [:T₂, :W₁, :W₂, :W₃]),
-    )
-    estimands = [
-        ATE(
-            scm,
-            outcome=:Y₁, 
-            treatment=(T₁=(case=1, control=0), T₂=(case="AC", control="CC")),
-        ),
-        IATE(
-            scm,
-            outcome=:Y₁, 
-            treatment=(T₁=(case=1, control=0), T₂=(case="AA", control="CC")),
-        ),
-        ATE(
-            scm,
-            outcome=:Y₁, 
-            treatment=(T₁=(case=1, control=0),),
-        ),
-        ATE(
-            scm,
-            outcome=:Y₂, 
-            treatment=(T₂=(case="AC", control="CC"),),
-        ),
-        CM(
-            scm,
-            outcome=:Y₂, 
-            treatment=(T₂="AC",),
-        ),
-        IATE(
-            scm,
-            outcome=:Y₁, 
-            treatment=(T₁=(case=0, control=1), T₂=(case="AC", control="CC"),),
-        ),
-        CM(
-            scm,
-            outcome=:Y₂, 
-            treatment=(T₂="CC",),
-        ),
-        ATE(
-            scm,
-            outcome=:Y₂, 
-            treatment=(T₂=(case="AA", control="CC"),),
-        ),
-        ATE(
-            scm,
-            outcome=:Y₂, 
-            treatment=(T₂=(case="AA", control="AC"),),
-        ),
-    ]
-    # Test estimand_key
-    @test TMLE.estimand_key(estimands[1]) == ("T₁_T₂", "Y₁")
-    @test TMLE.estimand_key(estimands[end]) == ("T₂", "Y₂")
-    # Non mutating function
-    estimands = shuffle(rng, estimands)
-    ordered_estimands = optimize_ordering(estimands)
-    expected_ordering = [
-        # Y₁
-        ATE(scm, outcome=:Y₁, treatment=(T₁ = (case = 1, control = 0),)),
-        ATE(scm, outcome=:Y₁, treatment=(T₁ = (case = 1, control = 0),T₂ = (case = "AC", control = "CC"))),
-        IATE(scm, outcome=:Y₁, treatment=(T₁ = (case = 0, control = 1), T₂ = (case = "AC", control = "CC"),)),
-        IATE(scm, outcome=:Y₁, treatment=(T₁ = (case = 1, control = 0),T₂ = (case = "AA", control = "CC"))),
-        # Y₂
-        ATE(scm, outcome=:Y₂, treatment=(T₂ = (case = "AA", control = "AC"),)),
-        CM(scm, outcome=:Y₂, treatment=(T₂ = "CC",)),
-        ATE(scm, outcome=:Y₂, treatment=(T₂ = (case = "AA", control = "CC"),)),
-        CM(scm, outcome=:Y₂, treatment=(T₂ = "AC",)),
-        ATE(scm, outcome=:Y₂, treatment=(T₂ = (case = "AC", control = "CC"),)),
-    ]
-    @test ordered_estimands == expected_ordering
-    # Mutating function
-    optimize_ordering!(estimands)
-    @test estimands == ordered_estimands
-end
-
-@testset "Test indicator_fns & indicator_values" begin
-    scm = StaticConfoundedModel(
-        [:Y],
-        [:T₁, :T₂, :T₃],
-        [:W]
-    )
+@testset "Test CMCompositeEstimand" begin
     dataset = (
         W  = [1, 2, 3, 4, 5, 6, 7, 8],
         T₁ = ["A", "B", "A", "B", "A", "B", "A", "B"],
@@ -268,32 +19,38 @@ end
     )
     # Counterfactual Mean
     Ψ = CM(
-        scm,
         outcome=:Y, 
-        treatment=(T₁="A", T₂=1),
+        treatment_values=(T₁="A", T₂=1),
+        treatment_confounders=(T₁=[:W], T₂=[:W])
     )
     indicator_fns = TMLE.indicator_fns(Ψ)
     @test indicator_fns == Dict(("A", 1) => 1.)
-    indic_values = TMLE.indicator_values(indicator_fns, TMLE.treatments(dataset, Ψ))
+    indic_values = TMLE.indicator_values(indicator_fns, TMLE.selectcols(dataset, TMLE.treatments(Ψ)))
     @test indic_values == [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0]
     # ATE
     Ψ = ATE(
-        scm,
         outcome=:Y, 
-        treatment=(T₁=(case="A", control="B"), T₂=(control=0, case=1)),
+        treatment_values=(
+            T₁=(case="A", control="B"), 
+            T₂=(control=0, case=1)
+        ),
+        treatment_confounders=(T₁=[:W], T₂=[:W])
     )
     indicator_fns = TMLE.indicator_fns(Ψ)
     @test indicator_fns == Dict(
         ("A", 1) => 1.0,
         ("B", 0) => -1.0
     )
-    indic_values = TMLE.indicator_values(indicator_fns, TMLE.treatments(dataset, Ψ))
+    indic_values = TMLE.indicator_values(indicator_fns, TMLE.selectcols(dataset, TMLE.treatments(Ψ)))
     @test indic_values == [0.0, -1.0, 1.0, 0.0, 0.0, -1.0, 1.0, 0.0]
     # 2-points IATE
     Ψ = IATE(
-        scm,
         outcome=:Y, 
-        treatment=(T₁=(case="A", control="B"), T₂=(case=1, control=0)),
+        treatment_values=(
+            T₁=(case="A", control="B"), 
+            T₂=(case=1, control=0)
+        ),
+        treatment_confounders=(T₁=[:W], T₂=[:W])
     )
     indicator_fns = TMLE.indicator_fns(Ψ)
     @test indicator_fns == Dict(
@@ -302,13 +59,17 @@ end
         ("B", 1) => -1.0,
         ("B", 0) => 1.0
     )
-    indic_values = TMLE.indicator_values(indicator_fns, TMLE.treatments(dataset, Ψ))
+    indic_values = TMLE.indicator_values(indicator_fns, TMLE.selectcols(dataset, TMLE.treatments(Ψ)))
     @test indic_values == [-1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0]
     # 3-points IATE
     Ψ = IATE(
-        scm,
         outcome=:Y, 
-        treatment=(T₁=(case="A", control="B"), T₂=(case=1, control=0), T₃=(control="D", case="C")),
+        treatment_values=(
+            T₁=(case="A", control="B"), 
+            T₂=(case=1, control=0), 
+            T₃=(control="D", case="C")
+        ),
+        treatment_confounders=(T₁=[:W], T₂=[:W], T₃=[:W])
     )
     indicator_fns = TMLE.indicator_fns(Ψ)
     @test indicator_fns == Dict(
@@ -321,7 +82,7 @@ end
         ("B", 1, "D") => 1.0,
         ("A", 0, "C") => -1.0
     )
-    indic_values = TMLE.indicator_values(indicator_fns, TMLE.treatments(dataset, Ψ))
+    indic_values = TMLE.indicator_values(indicator_fns, TMLE.selectcols(dataset, TMLE.treatments(Ψ)))
     @test indic_values == [-1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0]
 end
 
@@ -329,8 +90,6 @@ end
     @test isconcretetype(ATE)
     @test isconcretetype(IATE)
     @test isconcretetype(CM)
-    @test isconcretetype(TMLE.TMLEstimate{Float64})
-    @test isconcretetype(TMLE.OSEstimate{Float64})
 end
 
 end
