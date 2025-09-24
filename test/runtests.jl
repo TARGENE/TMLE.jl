ff6f894453dae2eb5f4d24b82bd467bebb676f9c
Asantewaah <41793350+Asantewaah@users.noreply.github.com>
2025-09-24 15:23:03 +0100

    Refactor test file to simplify includes

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,39 +1,38 @@
 using Test
 using TMLE
 
-TEST_DIR = joinpath(pkgdir(TMLE), "test")
+const TEST_DIR = joinpath(pkgdir(TMLE), "test")
+@testset "TMLE.jl" begin
+        # Test general functionality
+        include("utils.jl")
+        include("scm.jl")
+        include("adjustment.jl")
+        include("estimands.jl")
+        include("estimators_and_estimates.jl")
+        include("missing_management.jl")
+        include("composition.jl")
+        include("resampling.jl")
 
-@time begin
-    # Test general functionality
-    @test include(joinpath(TEST_DIR, "utils.jl"))
-    @test include(joinpath(TEST_DIR, "scm.jl"))
-    @test include(joinpath(TEST_DIR, "adjustment.jl"))
-    @test include(joinpath(TEST_DIR, "estimands.jl"))
-    @test include(joinpath(TEST_DIR, "estimators_and_estimates.jl"))
-    @test include(joinpath(TEST_DIR, "missing_management.jl"))
-    @test include(joinpath(TEST_DIR, "composition.jl"))
-    @test include(joinpath(TEST_DIR, "resampling.jl"))
-    
-    # Test Counterfactual Mean Based Estimation
-    @test include(joinpath(TEST_DIR, "counterfactual_mean_based/estimands.jl"))
-    @test include(joinpath(TEST_DIR, "counterfactual_mean_based/clever_covariate.jl"))
-    @test include(joinpath(TEST_DIR, "counterfactual_mean_based/gradient.jl"))
-    @test include(joinpath(TEST_DIR, "counterfactual_mean_based/fluctuation.jl"))
-    @test include(joinpath(TEST_DIR, "counterfactual_mean_based/estimators_and_estimates.jl"))
-    @test include(joinpath(TEST_DIR, "counterfactual_mean_based/non_regression_test.jl"))
-    @test include(joinpath(TEST_DIR, "counterfactual_mean_based/double_robustness_ate.jl"))
-    @test include(joinpath(TEST_DIR, "counterfactual_mean_based/double_robustness_aie.jl"))
-    @test include(joinpath(TEST_DIR, "counterfactual_mean_based/3points_interactions.jl"))
-    @test include(joinpath(TEST_DIR, "counterfactual_mean_based/collaborative_template.jl"))
-    @test include(joinpath(TEST_DIR, "counterfactual_mean_based/covariate_based_strategies.jl"))
-    @test include(joinpath(TEST_DIR, "counterfactual_mean_based/lasso_strategy.jl"))
-    
-    # Test Extensions
-    if VERSION >= v"1.9"
-        @test include(joinpath(TEST_DIR, "configuration.jl"))
-        @test include(joinpath(TEST_DIR, "causaltables_interface.jl"))
-    end
+        # Test Counterfactual Mean Based Estimation
+        include("counterfactual_mean_based/estimands.jl")
+        include("counterfactual_mean_based/clever_covariate.jl")
+        include("counterfactual_mean_based/gradient.jl")
+        include("counterfactual_mean_based/fluctuation.jl")
+        include("counterfactual_mean_based/estimators_and_estimates.jl")
+        include("counterfactual_mean_based/non_regression_test.jl")
+        include("counterfactual_mean_based/double_robustness_ate.jl")
+        include("counterfactual_mean_based/double_robustness_aie.jl")
+        include("counterfactual_mean_based/3points_interactions.jl")
+        include("counterfactual_mean_based/collaborative_template.jl")
+        include("counterfactual_mean_based/covariate_based_strategies.jl")
+        include("counterfactual_mean_based/lasso_strategy.jl")
 
-    # Test Experimental
-    @test include(joinpath(TEST_DIR, "estimand_ordering.jl"))
+        # Test Extensions
+        if VERSION >= v"1.9"
+            include("configuration.jl")
+            include("causaltables_interface.jl")
+        end
+
+        # Test Experimental
+        include("estimand_ordering.jl")
 end
