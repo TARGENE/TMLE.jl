61fba8d844dc13c8146ce5b2d5597184dc95edce
roskamsh <breesh.roskamshieter@gmail.com>
2026-04-07 15:08:01 +0100

    update check to look for prevalence file

--- src/utils.jl
+++ src/utils.jl
@@ -253,9 +253,9 @@ with_encoder(model; encoder=ContinuousEncoder(drop_last=true, one_hot_ordered_fa
 
 Evaluate if the dataset is suitable for the estimand Ψ.
 """
-function check_inputs(Ψ, dataset, prevalence)
+function check_inputs(Ψ, dataset, prevalence, prevalence_file)
     check_treatment_levels(Ψ, dataset)
-    !isnothing(prevalence) && ccw_check(dataset, Ψ.outcome)
+    (!isnothing(prevalence) || !isnothing(prevalence_file)) && ccw_check(dataset, Ψ.outcome)
 end
 
 """
